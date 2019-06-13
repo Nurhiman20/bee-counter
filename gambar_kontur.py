535753cfe5293236f501ad46b9bd49daac78a597
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2019-06-13 14:54:54 +0700

    Menambahkan program menggambar kontur

---
+++ gambar_kontur.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    # frame = cv x2.imread('smarties.png')
+    _, frame = cap.read()
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    l_b = np.array([34, 50, 30])
+    u_b = np.array([84, 255, 106])
+    mask = cv2.inRange(hsv, l_b, u_b)
+
+    _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
+
+    # res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask)
+    # cv2.imshow("res", res)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
