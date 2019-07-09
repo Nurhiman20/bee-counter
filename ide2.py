a7929abf7af597ad5c84fd84399664c7bdeed0c0
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2019-07-09 17:10:04 +0700

    coba algoritma baru (belum berhasil)

---
+++ ide2.py
@@ -0,0 +1,168 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+fps, width, height = cap.get(cv2.CAP_PROP_FPS), cap.get(
+    cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+width = int(width)
+height = int(height)
+print(fps, width, height)
+
+while True :
+    # mengambil citra
+    _, frame = cap.read()
+
+    # konversi warna BGR ke HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # definisikan area pertama
+    lineypos = 225
+    cv2.line(frame, (0, lineypos), (width, lineypos), (255, 0, 0), 2)
+    lineypos2 = 235
+    cv2.line(frame, (0, lineypos2), (width, lineypos2), (255, 0, 0), 2)
+    
+    # definisikan area kedua
+    lineypos3 = 400
+    cv2.line(frame, (0, lineypos3), (width, lineypos3), (0, 255, 0), 2)
+    lineypos4 = 410
+    cv2.line(frame, (0, lineypos4), (width, lineypos4), (0, 255, 0), 2)
+    
+    # definiskan range warna hijau
+    l_hijau = np.array([34, 50, 30])
+    u_hijau = np.array([89, 255, 95])
+
+    # definiskan range warna kuning
+    l_kuning = np.array([14, 110, 85])
+    u_kuning = np.array([55, 255, 139])
+
+    # definiskan range warna merah
+    l_merah = np.array([176, 90, 0])
+    u_merah = np.array([255, 212, 121])
+
+    # menemukan range warna pada citra
+    hijau = cv2.inRange(hsv, l_hijau, u_hijau)
+    kuning = cv2.inRange(hsv, l_kuning, u_kuning)
+    merah = cv2.inRange(hsv, l_merah, u_merah)
+
+    # morphological transformation, opening
+    kernel = np.ones((15,15), np.float32)/255
+    
+    hijau = cv2.morphologyEx(hijau, cv2.MORPH_OPEN, kernel)
+    res = cv2.bitwise_and(frame, frame, mask = hijau)
+
+    kuning = cv2.morphologyEx(kuning, cv2.MORPH_OPEN, kernel)
+    res1 = cv2.bitwise_and(frame, frame, mask = kuning)
+
+    merah = cv2.morphologyEx(merah, cv2.MORPH_OPEN, kernel)
+    res2 = cv2.bitwise_and(frame, frame, mask = merah)
+
+    # batas ukuran tagging yang terbaca
+    minarea = 10
+    maxarea = 50000
+    
+    # tracking warna hijau
+    contours,hierachy = cv2.findContours(hijau, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
+
+    cxx = np.zeros(len(contours))
+    cyy = np.zeros(len(contours))
+
+    for i in range(len(contours)):  # looping pada semua kontur dalam frame
+
+        if hierachy[0, i, 3] == -1:  # hierachy untuk hanya menghitung pada kontur induk
+
+            area = cv2.contourArea(contours[i])  # luas kontur
+
+            if minarea < area < maxarea:
+                # menghitung centroid kontur
+                cnt = contours[i]
+                M = cv2.moments(cnt)
+                cx = int(M['m10'] / M['m00'])
+                cy = int(M['m01'] / M['m00'])
+
+                if lineypos < cy < lineypos2:   # kirim data saat melewati area pertama
+                    print(area)
+
+                elif cy > lineypos:  # menyeleksi kontur yang sudah melewati garis
+
+                    # mendapatkan nilai titik sudut untuk menggambar kontur persegi
+                    # x,y adalah sudut kiri atas dan w,h adalah lebar dan tinggi
+                    x, y, w, h = cv2.boundingRect(cnt)
+
+                    # membuat kotak yang melingkupi setiap kontur
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (121,43,236), 2)
+    
+    # tracking warna kuning
+    contours,hierachy = cv2.findContours(kuning, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
+
+    cxx = np.zeros(len(contours))
+    cyy = np.zeros(len(contours))
+
+    for i in range(len(contours)):  # looping pada semua kontur dalam frame
+
+        if hierachy[0, i, 3] == -1:  # hierachy untuk hanya menghitung pada kontur induk
+
+            area = cv2.contourArea(contours[i])  # luas kontur
+
+            if minarea < area < maxarea:
+                # menghitung centroid kontur
+                cnt = contours[i]
+                M = cv2.moments(cnt)
+                cx = int(M['m10'] / M['m00'])
+                cy = int(M['m01'] / M['m00'])
+
+                if lineypos < cy < lineypos2:   # kirim data saat melewati area pertama
+                    print(area)
+
+                elif cy > lineypos:  # menyeleksi kontur yang sudah melewati garis
+
+                    # mendapatkan nilai titik sudut untuk menggambar kontur persegi
+                    # x,y adalah sudut kiri atas dan w,h adalah lebar dan tinggi
+                    x, y, w, h = cv2.boundingRect(cnt)
+
+                    # membuat kotak yang melingkupi setiap kontur
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (121,43,236), 2)
+
+    # tracking warna merah
+    contours,hierachy = cv2.findContours(merah, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
+
+    cxx = np.zeros(len(contours))
+    cyy = np.zeros(len(contours))
+
+    for i in range(len(contours)):  # looping pada semua kontur dalam frame
+
+        if hierachy[0, i, 3] == -1:  # hierachy untuk hanya menghitung pada kontur induk
+
+            area = cv2.contourArea(contours[i])  # luas kontur
+
+            if minarea < area < maxarea:
+                # menghitung centroid kontur
+                cnt = contours[i]
+                M = cv2.moments(cnt)
+                cx = int(M['m10'] / M['m00'])
+                cy = int(M['m01'] / M['m00'])
+
+                if lineypos < cy < lineypos2:   # kirim data saat melewati area pertama
+                    print(area)
+
+                elif cy > lineypos:  # menyeleksi kontur yang sudah melewati garis
+
+                    # mendapatkan nilai titik sudut untuk menggambar kontur persegi
+                    # x,y adalah sudut kiri atas dan w,h adalah lebar dan tinggi
+                    x, y, w, h = cv2.boundingRect(cnt)
+
+                    # membuat kotak yang melingkupi setiap kontur
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (121,43,236), 2)
+
+    cv2.imshow("frame", frame)
+
+    # menutup frame / program
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
